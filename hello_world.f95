909725f48859fa190191cb69412914bc3772d3fc
joaovitoralvesportela <portelajose51@gmail.com>
2026-07-12 17:46:32 -0300

    Hello World Fortran95

---
+++ hello_world.f95
@@ -0,0 +1,8 @@
+
+PROGRAM HELLO  ! Declares the start of a Fortran program.
+
+	PRINT *, 'Hello World!'  ! This print a mensage Wow!
+	! (PRINT) -> instruction that displays something on the console.
+	! (*) -> standard formatting
+	! (Hello World) -> a string literal that will be printed
+END PROGRAM HELLO ! (END PROGRAM <NAME>) -> Marks the end of the program.
---
+++ teste_fortran_inicial.f95
@@ -0,0 +1,12 @@
+
+PROGRAM TESTE
+	! Programa de teste do gfortran
+	IMPLICIT NONE 
+	
+	INTEGER :: n
+	WRITE(*,*) 'Digite um numero:'
+	READ(*,*) n
+	WRITE(*,*) 'O numero que voce escreveu foi: ', n
+END PROGRAM TESTE
+
+
