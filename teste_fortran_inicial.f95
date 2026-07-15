ba65d50802f88697f8f2c2c804abd78233f3078e
joaovitoralvesportela <portelajose51@gmail.com>
2026-07-14 22:12:56 -0300

    Enhance comments in Fortran test program
    Added comments to clarify the Fortran program structure and functionality.
    -> Kommentare hinzugefügt, um die Struktur und Funktionalität des Fortran-Programms zu verdeutlichen.

--- teste_fortran_inicial.f95
+++ teste_fortran_inicial.f95
@@ -1,12 +1,11 @@
-
-PROGRAM TESTE
-	! Programa de teste do gfortran
-	IMPLICIT NONE 
+PROGRAM TESTE !Declare the start of a Fortran program named TESTE; every program must begin with the PROGRAM statement.
 	
-	INTEGER :: n
-	WRITE(*,*) 'Digite um numero:'
-	READ(*,*) n
-	WRITE(*,*) 'O numero que voce escreveu foi: ', n
-END PROGRAM TESTE
+	IMPLICIT NONE ! This line disables Fortran's "implicit typing."
+	
+	INTEGER :: n ! Declares the variable n as an integer. The syntax TYPE :: name is the Fortran 90+ standard.
+	WRITE(*,*) 'Digite um numero:' !  Exit command. The first * indicates that the output goes to the standard device.
+	READ(*,*) n ! Input (read) command. The first * indicates that the input comes from the standard device (keyboard).
+	WRITE(*,*) 'O numero que voce escreveu foi: ', n ! Another output command. Prints the text followed by the value of the variable n.
+END PROGRAM TESTE ! Marks the end of the program.
 
 
